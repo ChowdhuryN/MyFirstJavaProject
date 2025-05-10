151b53708fd376478467f916b43df723b0453614
NazimC <chowdhurynazim14@gmail.com>
2025-05-10 09:24:38 -0700

    Java Assignment_02 Submitted

---
+++ src/Assignment_02/assignment.java
@@ -0,0 +1,22 @@
+package Assignment_02;
+
+public class assignment {
+    public static void main(String[] args) {
+        int num1 = 25,num2= 35, num3;
+
+        num1 += num2; // num1 = num1 + num2
+        System.out.println("Addition:" + num1);
+
+        num2 -= num1; // num2 = num2 - num1
+        System.out.println("Subtraction:" + num2);
+
+        num1 *= num2; // num1 = num1 * num2
+        System.out.println("Multiplication:" + num1);
+
+        num1 /= num1; // num1 = num1 / num1
+        System.out.println("Division:" + num1);
+
+
+
+}
+}
---
+++ src/Assignment_02/ternary.java
@@ -0,0 +1,14 @@
+package Assignment_02;
+
+public class ternary {
+    public static void main(String[] args) {
+
+        int num12 = 125, num16 = 200;
+
+        // Ternary operator
+         int max = (num12 > num16) ? num12 : num16;
+        System.out.println("Maximum number is: " + max);
+
+
+    }
+}
